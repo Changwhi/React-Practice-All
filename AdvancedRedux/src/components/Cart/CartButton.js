447c91fa662286e98e355a5b66df659c9b431334
Changwhi <whinada@gmail.com>
2023-08-24 15:25:39 -0700

    Feat: Impelment a toggle function on the MyCart button

--- AdvancedRedux/src/components/Cart/CartButton.js
+++ AdvancedRedux/src/components/Cart/CartButton.js
@@ -1,12 +1,19 @@
 import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux';
+import { cartAction } from '../../Store/cart';
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+    const dispatch = useDispatch();
+    const cartToggleHandler = () => {
+        dispatch(cartAction.toggle())
+    };
+
+    return (
+        <button onClick={cartToggleHandler} className={classes.button}>
+            <span>My Cart</span>
+            <span className={classes.badge}>1</span>
+        </button>
+    );
 };
 
 export default CartButton;
