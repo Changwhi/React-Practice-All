ccfd1963d552ff8b5c44df59148c918c557ccb8b
Changwhi <whinada@gmail.com>
2023-08-24 18:23:08 -0700

    Refactor: A batch number changes based on the number of items in a cart

--- AdvancedRedux/src/components/Cart/CartButton.js
+++ AdvancedRedux/src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
 import classes from './CartButton.module.css';
 import { useDispatch } from 'react-redux';
 import { cartAction } from '../../Store/cart';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 const CartButton = (props) => {
+    const [quantity, setQuantity] = useState(0);
+    const items = useSelector(state => state.cart.items);
+    items.map(item => setQuantity(prev => {return prev+item.quantity}))
+
+
     const dispatch = useDispatch();
     const cartToggleHandler = () => {
         dispatch(cartAction.toggle())
@@ -11,7 +18,7 @@ const CartButton = (props) => {
     return (
         <button onClick={cartToggleHandler} className={classes.button}>
             <span>My Cart</span>
-            <span className={classes.badge}>1</span>
+            <span className={classes.badge}>{quantity}</span>
         </button>
     );
 };
