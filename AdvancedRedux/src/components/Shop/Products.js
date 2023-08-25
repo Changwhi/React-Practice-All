472702346f148d521adf32753096e09a12bb16cc
Changwhi <whinada@gmail.com>
2023-08-25 12:37:11 -0700

    Feat: Add one more option(product)

--- AdvancedRedux/src/components/Shop/Products.js
+++ AdvancedRedux/src/components/Shop/Products.js
@@ -1,19 +1,34 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY = [{
+    itemId: 1,
+    title: 'Test1',
+    price: 6,
+    description: 'This is a first product - amazing!'
+},
+{
+    itemId:2,
+    title: 'Test2',
+    price: 2,
+    description: 'This is a second product - amazing!'
+}]
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY.map(book => {
+                    return <ProductItem
+                        itemId={book.itemId}
+                        title={book.title}
+                        price={book.price}
+                        description={book.description}
+                    />
+                })}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
