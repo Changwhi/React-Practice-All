ea8996d9cf2171fdeec595ddb11f1cb2563bf7ba
Changwhi <whinada@gmail.com>
2023-08-24 17:51:35 -0700

    Feat: Implement reducer for increment quantity

--- AdvancedRedux/src/Store/cart.js
+++ AdvancedRedux/src/Store/cart.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {isExpanded: false}
+const initialState = {isExpanded: false, items:[], totalAmount: 0}
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -8,6 +8,30 @@ const cartSlice = createSlice({
     reducers:{
         toggle(state) {
             state.isExpanded = !state.isExpanded
+        },
+        increment(state, action) {
+            const currentItem = state.items.find(item => item.itemId === action.payload.itemId)
+
+            if (!currentItem) {
+                state.items.push({
+                    price: action.payload.price,
+                    itemId: action.payload.itemId,
+                    quantity: 1,
+                    title: action.payload.title,
+                    content: action.payload.content,
+                    totalAmount: action.payload.totalAmount
+                })
+            }
+            else {
+                currentItem.totalAmount = currentItem.totalAmount + currentItem.price;
+                currentItem.quantity++;
+            }
+        },
+        decrement(state) {
+
+        },
+        addToCart(state) {
+
         }
     }
 });
