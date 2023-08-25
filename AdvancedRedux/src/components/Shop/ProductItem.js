ff310bc965e27b642dcf27187ca838462d58286f
Changwhi <whinada@gmail.com>
2023-08-25 12:02:52 -0700

    Feat: implement a function in a addToCart button

--- AdvancedRedux/src/components/Shop/ProductItem.js
+++ AdvancedRedux/src/components/Shop/ProductItem.js
@@ -1,8 +1,21 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import { cartAction } from '../../Store/cart';
+import { useDispatch } from 'react-redux';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { itemId, title, price, description } = props;
+  const dispatch = useDispatch();
+
+    const addToCartHandler = () => {
+        dispatch(cartAction.addToCart({
+            price: price,
+            itemId: itemId,
+            quantity: 1,
+            title: title,
+            content: description
+        }))
+    };
 
   return (
     <li className={classes.item}>
@@ -13,7 +26,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
