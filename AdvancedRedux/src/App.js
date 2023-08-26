a4f9aa0a815ad169d12aa79a3e936aea671cf59e
Changwhi <whinada@gmail.com>
2023-08-26 15:41:10 -0700

    Feat: Stores items to Firebase

--- AdvancedRedux/src/App.js
+++ AdvancedRedux/src/App.js
@@ -2,16 +2,33 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 
 function App() {
     const isExpanded = useSelector(state => state.cart.isExpanded)
-  return (
-    <Layout>
-      {isExpanded && <Cart />}
-      <Products />
-    </Layout>
-  );
+    const cart = useSelector(state => state.cart.items)
+
+    useEffect(() => {
+        const response = async () => {
+            await fetch('https://react-food-ordering-ff3f4-default-rtdb.firebaseio.com/cart.json',
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(cart)
+                })
+        };
+        response().catch(err => {
+            console.log(err)
+        })
+    }, [cart]);
+
+
+    return (
+        <Layout>
+            {isExpanded && <Cart />}
+            <Products />
+        </Layout>
+    );
 }
 
 export default App;
