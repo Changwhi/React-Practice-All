43088fb586a26efd8e0440ae92cc3c5d5e7fc430
Changwhi <whinada@gmail.com>
2023-08-29 19:09:15 -0700

    Feat: Implement Link to product page from the main page

--- MultipleSPA/src/Components/Main.js
+++ MultipleSPA/src/Components/Main.js
@@ -1,5 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Main = () => {
-    return <p>Test</p>
+    return <React.Fragment>
+        <p>Test</p>
+        <Link to="/product"> Link to product page </Link>
+    </React.Fragment>
 };
 
 export default Main;
