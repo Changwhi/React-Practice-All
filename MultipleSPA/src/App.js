3281de7065742fb8bd877473dedcaa7edc4f9e26
Changwhi <whinada@gmail.com>
2023-09-06 13:19:58 -0700

    Feat: Adds errorElement property

--- MultipleSPA/src/App.js
+++ MultipleSPA/src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Main from './pages/Main';
 import Product from './pages/Product';
 import Roots from './pages/Roots';
+import Error from './pages/Error';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Roots />,
+        errorElement: <Error />,
         children: [
             {path: '/', element:<Main />},
             {path: '/product', element:<Product />},
