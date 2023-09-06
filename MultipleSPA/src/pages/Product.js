314ccc54fc4f1c9c2fe6d90e58ff98ce404db24c
Changwhi <whinada@gmail.com>
2023-09-06 14:28:37 -0700

    Feat: Adds products list and links to respective routes

--- MultipleSPA/src/pages/Product.js
+++ MultipleSPA/src/pages/Product.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
+const DUMMY = [
+    {productId: 1, productName: 'product1'},
+    {productId: 2, productName: 'product2'},
+    {productId: 3, productName: 'product3'},
+]
+
+
 const Product = () => {
-    return <h2>Product page</h2>
+
+    return <>
+        <h2>Product page</h2>
+        <ul>
+        {DUMMY.map((product) =>
+            <li><Link to={`/product/${product.productId}`}>{product.productName}</Link></li>
+            )}
+        </ul>
+        </>
 };
 
 
